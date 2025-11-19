3ef7b051836ce3e165fe3fa3b5b95eab54e52aef
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-11-18 20:38:43 -0600

    Added error handling middleware

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,6 +1,6 @@
 // src/app.ts
 import express, {json, urlencoded} from "express";
-import { Response as ExResponse, Request as ExRequest } from "express";
+import { Response as ExResponse, Request as ExRequest, NextFunction } from "express";
 
 import { RegisterRoutes } from "../build/routes";
 
@@ -9,6 +9,7 @@ export const app = express();
 // src/app.ts
 
 import swaggerUi from "swagger-ui-express";
+import { ValidateError } from "tsoa";
 
 // ...
 
@@ -30,5 +31,30 @@ RegisterRoutes(app);
 
 
 
+app.use(function errorHandler(
+  err: unknown,
+  req: ExRequest,
+  res: ExResponse,
+  next: NextFunction
+): ExResponse | void {
+  if (err instanceof ValidateError) {
+    console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
+    return res.status(422).json({
+      message: "Validation Failed",
+      details: err?.fields,
+    });
+  }
+  if (err instanceof Error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 
+  next();
+});
 
+app.use(function notFoundHandler(_req, res: ExResponse) {
+  res.status(404).send({
+    message: "Not Found",
+  });
+});
